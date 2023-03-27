df5ee5ba1d1c5d78f9670dff8ee23f9910ff7a34
disasetr37 <linuxworkgroup@hotmail.com>
2023-03-27 13:56:37 +0000

    fix: add loop to avoid crash container
    Signed-off-by: disasetr37 <linuxworkgroup@hotmail.com>

--- smee-client.go
+++ smee-client.go
@@ -7,7 +7,6 @@ import (
 	"crypto/tls"
 	"encoding/hex"
 	"errors"
-	"fmt"
 	"net/http"
 	"strings"
 	"time"
@@ -44,122 +43,120 @@ func startSmee(c *cli.Context) error {
 		return errors.New("--target parameter is required")
 	}
 
-	clientSmee := &http.Client{
-		Timeout: c.Duration("timeout") * time.Second,
-	}
-	clientBackend := &http.Client{
-		Timeout: c.Duration("timeout") * time.Second,
-	}
+	var (
+		clientSmee    *http.Client
+		clientBackend *http.Client
+	)
 
-	if c.Bool("self-signed-certificate") {
-		clientBackend.Transport = &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	// Loop here to avoid CrashLoopBackOff on Kube
+	for {
+		clientSmee = &http.Client{
+			Timeout: c.Duration("timeout") * time.Second,
 		}
-	}
-
-	// Check we can access on URL
-	_, err := clientSmee.Get(c.String("url"))
-	if err != nil {
-		return fmt.Errorf("error when access on URL %s: %s", c.String("url"), err.Error())
-	}
-
-	// Check we can access on backend
-	// Maybee we need to wait some time before backend start
-	wait := time.After(c.Duration("timeout"))
-	loop := true
-	for loop {
-		select {
-		case <-wait:
-			return fmt.Errorf("we can't access on target during %d seconds", c.Duration("timeout")/time.Second)
-		default:
-			_, err = clientBackend.Get(c.String("target"))
-			if err != nil {
-				log.Warnf("Error when access on target %s: %s", c.String("target"), err.Error())
-				time.Sleep(30 * time.Second)
-			} else {
-				loop = false
-			}
+		clientBackend = &http.Client{
+			Timeout: c.Duration("timeout") * time.Second,
 		}
-	}
 
-	// Disable timeout for live stream
-	clientSmee.Timeout = 0
-	evCh := make(chan *Event)
-	go Notify(clientSmee, c.String("url"), evCh)
-
-	log.Infof("We proxy '%s' to '%s'", c.String("url"), c.String("target"))
-
-	for ev := range evCh {
-
-		// Handle error event
-		if ev.Err != nil {
-			switch ev.Err {
-			case ErrNilChan:
-				log.Errorf("You need to provide chan")
-				return ev.Err
-			case ErrLostConnexion:
-				log.Warnf("We lost connexion on %s, we try to reconnect on it", c.String("url"))
-				time.Sleep(c.Duration("timeout"))
-				go Notify(clientSmee, c.String("url"), evCh)
-			default:
-				log.Errorf("Error appear: %s", ev.Err.Error())
-				time.Sleep(c.Duration("timeout"))
-				go Notify(clientSmee, c.String("url"), evCh)
+		if c.Bool("self-signed-certificate") {
+			clientBackend.Transport = &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 			}
 		}
 
-		// Handle data event
-		if len(ev.Data) <= 2 {
-			continue
-		}
-
-		body, _, _, err := jsonparser.Get(ev.Data, "body")
+		// Check we can access on proxy URL
+		_, err := clientSmee.Get(c.String("url"))
 		if err != nil {
-			log.Info("Error: no body found")
+			log.Errorf("Error when access on URL %s: %s", c.String("url"), err.Error())
+			time.Sleep(c.Duration("timeout"))
 			continue
 		}
 
-		if c.String("secret") != "" {
-			signature, _, _, err := jsonparser.Get(ev.Data, "x-hub-signature")
-			if err != nil {
-				log.Info("Error: no signature found")
-				continue
-			}
-			if string(signature[:5]) != "sha1=" {
-				log.Warnf("Skipping checking. signature is not SHA1: %s\n", signature)
-				continue
-			} else {
-				if !ValidMAC([]byte(body), hex2bytes(string(signature[5:])), []byte(c.String("secret"))) {
-					log.Error("Error: Invalid HMAC\n")
-					continue
+		// Check we can access on backend URL
+		_, err = clientBackend.Get(c.String("target"))
+		if err != nil {
+			log.Errorf("Error when access on target %s: %s", c.String("target"), err.Error())
+			time.Sleep(c.Duration("timeout"))
+			continue
+		}
+
+		// Disable timeout for live stream
+		clientSmee.Timeout = 0
+		evCh := make(chan *Event)
+		go Notify(clientSmee, c.String("url"), evCh)
+		log.Infof("We proxy '%s' to '%s'", c.String("url"), c.String("target"))
+
+		for ev := range evCh {
+
+			// Handle error event
+			if ev.Err != nil {
+				switch ev.Err {
+				case ErrNilChan:
+					log.Errorf("You need to provide chan")
+					return ev.Err
+				case ErrLostConnexion:
+					log.Warnf("We lost connexion on %s, we try to reconnect on it", c.String("url"))
+					time.Sleep(c.Duration("timeout"))
+					go Notify(clientSmee, c.String("url"), evCh)
+				default:
+					log.Errorf("Error appear: %s", ev.Err.Error())
+					time.Sleep(c.Duration("timeout"))
+					go Notify(clientSmee, c.String("url"), evCh)
 				}
 			}
-		}
 
-		// Call target to send event
-		req, err := http.NewRequest("POST", c.String("target"), bytes.NewBuffer(body))
-		if err != nil {
-			log.Errorf("Error when create request: %s", err.Error())
-			continue
-		}
-		err = jsonparser.ObjectEach(ev.Data, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
-			if strings.HasPrefix(string(key), "x-") || strings.ToLower(string(key)) == "content-type" {
-				req.Header.Set(string(key), string(value))
+			// Handle data event
+			if len(ev.Data) <= 2 {
+				continue
 			}
-			return nil
-		})
-		if err != nil {
-			log.Errorf("Error when set header: %s", err.Error())
-			continue
-		}
 
-		_, err = clientBackend.Do(req)
-		if err != nil {
-			log.Errorf("Error when call target: %s", err.Error())
-			continue
-		}
+			body, _, _, err := jsonparser.Get(ev.Data, "body")
+			if err != nil {
+				log.Info("Error: no body found")
+				continue
+			}
 
-		log.Infof("Successfully proxied webhook to target: %s", string(body))
+			if c.String("secret") != "" {
+				signature, _, _, err := jsonparser.Get(ev.Data, "x-hub-signature")
+				if err != nil {
+					log.Info("Error: no signature found")
+					continue
+				}
+				if string(signature[:5]) != "sha1=" {
+					log.Warnf("Skipping checking. signature is not SHA1: %s\n", signature)
+					continue
+				} else {
+					if !ValidMAC([]byte(body), hex2bytes(string(signature[5:])), []byte(c.String("secret"))) {
+						log.Error("Error: Invalid HMAC\n")
+						continue
+					}
+				}
+			}
+
+			// Call target to send event
+			req, err := http.NewRequest("POST", c.String("target"), bytes.NewBuffer(body))
+			if err != nil {
+				log.Errorf("Error when create request: %s", err.Error())
+				continue
+			}
+			err = jsonparser.ObjectEach(ev.Data, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+				if strings.HasPrefix(string(key), "x-") || strings.ToLower(string(key)) == "content-type" {
+					req.Header.Set(string(key), string(value))
+				}
+				return nil
+			})
+			if err != nil {
+				log.Errorf("Error when set header: %s", err.Error())
+				continue
+			}
+
+			_, err = clientBackend.Do(req)
+			if err != nil {
+				log.Errorf("Error when call target: %s", err.Error())
+				continue
+			}
+
+			log.Infof("Successfully proxied webhook to target: %s", string(body))
+		}
 	}
 
 	return nil
